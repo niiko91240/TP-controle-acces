2f90c15ab1744b620f339988af4924027d8e5cc1
Nicolas Anjorand <nicolasanjorand@MacBook-Air-de-Nicolas.local>
2024-02-09 13:55:23 +0100

    [RED] Cas 2 presentation badge

--- tests/test_controle_acces.py
+++ tests/test_controle_acces.py
@@ -36,5 +36,19 @@ class TestMain(unittest.TestCase):
         porte = PorteSpy()
         # QUAND le moteur d'ouverture interroge ce lecteur
         moteur = MoteurOuverture(porte)
+        moteur.interroger(lecteur)
         # ALORS cette porte ne s'ouvre pas
         self.assertFalse(porte.methode_ouvrir_appelee)
+
+    def test_cas_2_presentation(self):
+        # ETANT DONNE un lecteur ayant detecté un badge
+        lecteur = LecteurFake()
+        lecteur.simuler_presentation_badge()
+        # ET une porte lui étant liée
+        porte = PorteSpy()
+        # QUAND le moteur d'ouverture interroge deux fois ce lecteur
+        moteur = MoteurOuverture(porte)
+        moteur.interroger(lecteur)
+        moteur.interroger(lecteur)
+        # ALORS cette porte ne s'ouvre pas
+        self.assertEqual(1, porte.nb_appels_methode_ouvrir)
