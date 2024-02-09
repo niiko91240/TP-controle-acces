f9f5c0a25d40d4aeee5b6f4cd2ac6ee068c43b56
Nicolas Anjorand <nicolasanjorand@MacBook-Air-de-Nicolas.local>
2024-02-09 14:11:07 +0100

    [GREEN] Cas plusieurs portes

--- src/MoteurOuverture.py
+++ src/MoteurOuverture.py
@@ -2,9 +2,10 @@ from .ILecteur import ILecteur
 from .IPorte import IPorte
 
 class MoteurOuverture:
-    def __init__(self, porte: IPorte):
-        self._porte = porte
+    def __init__(self, *portes: IPorte):
+        self._portes = portes
 
     def interroger(self, lecteur):
         if lecteur.badge_detecte:
-            self._porte.ouvrir()
+            for porte in self._portes:
+                porte.ouvrir()
