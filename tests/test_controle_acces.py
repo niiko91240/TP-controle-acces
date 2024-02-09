5452f526d381039b616b136685e3caffb6e76108
Nicolas Anjorand <nicolasanjorand@MacBook-Air-de-Nicolas.local>
2024-02-09 13:50:25 +0100

    [GREEN] Cas sans interrogation

--- tests/test_controle_acces.py
+++ tests/test_controle_acces.py
@@ -17,3 +17,14 @@ class TestMain(unittest.TestCase):
         moteur.interroger(lecteur)
         # ALORS cette porte s'ouvre
         self.assertTrue(porte.methode_ouvrir_appelee)
+
+    def test_cas_sans_interrogation(self):
+        # ETANT DONNE un lecteur ayant détecté un badge
+        lecteur = LecteurFake()
+        lecteur.simuler_presentation_badge()
+        # ET une porte lui étant liée
+        porte = PorteSpy()
+        # QUAND le moteur d'ouverture interroge ce lecteur
+        moteur = MoteurOuverture(porte)
+        # ALORS cette porte ne s'ouvre pas
+        self.assertFalse(porte.methode_ouvrir_appelee)
