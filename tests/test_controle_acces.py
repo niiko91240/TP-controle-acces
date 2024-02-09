1038f7bfac6415ceb4f635d80f116e85a73d6614
Nicolas Anjorand <nicolasanjorand@macbook-air-de-nicolas.home>
2024-02-09 23:34:57 +0100

    Fix all tests

--- tests/test_controle_acces.py
+++ tests/test_controle_acces.py
@@ -11,86 +11,93 @@ class TestMain(unittest.TestCase):
 
     def test_cas_nominal(self):
         # ETANT DONNE un lecteur ayant détecté un badge
-        lecteur = LecteurFake()
-        lecteur.simuler_presentation_badge()
+        lecteur = LecteurFake(1)
+        lecteur.simuler_presentation_badge(1)
         # ET une porte lui étant liée
         porte = PorteSpy()
+        log = LogSpy()
         # QUAND le moteur d'ouverture interroge ce lecteur
-        moteur = MoteurOuverture(porte)
-        moteur.interroger(lecteur)
+        moteur = MoteurOuverture(log, porte)
+        moteur.interroger([lecteur])
         # ALORS cette porte s'ouvre
         self.assertEqual(1, porte.nb_appels_methode_ouvrir)
 
     def test_cas_sans_interrogation(self):
         # ETANT DONNE un lecteur ayant détecté un badge
-        lecteur = LecteurFake()
-        lecteur.simuler_presentation_badge()
+        lecteur = LecteurFake(1)
+        lecteur.simuler_presentation_badge(1)
         # ET une porte lui étant liée
         porte = PorteSpy()
+        log = LogSpy()
         # QUAND le moteur d'ouverture interroge ce lecteur
-        moteur = MoteurOuverture(porte)
+        moteur = MoteurOuverture(log,porte)
         # ALORS cette porte ne s'ouvre pas
         self.assertEqual(0, porte.nb_appels_methode_ouvrir)
 
     def test_cas_sans_presentation_badge(self):
         # ETANT DONNE un lecteur
-        lecteur = LecteurFake()
+        lecteur = LecteurFake(1)
         # ET une porte lui étant liée
         porte = PorteSpy()
+        log = LogSpy()
         # QUAND le moteur d'ouverture interroge ce lecteur
-        moteur = MoteurOuverture(porte)
-        moteur.interroger(lecteur)
+        moteur = MoteurOuverture(log, porte)
+        moteur.interroger([lecteur])
         # ALORS cette porte ne s'ouvre pas
         self.assertEqual(0, porte.nb_appels_methode_ouvrir)
 
     def test_cas_2_presentation(self):
         # ETANT DONNE un lecteur ayant detecté un badge
-        lecteur = LecteurFake()
-        lecteur.simuler_presentation_badge()
+        lecteur = LecteurFake(1)
+        lecteur.simuler_presentation_badge(1)
         # ET une porte lui étant liée
         porte = PorteSpy()
+        log = LogSpy()
         # QUAND le moteur d'ouverture interroge deux fois ce lecteur
-        moteur = MoteurOuverture(porte)
-        moteur.interroger(lecteur)
-        moteur.interroger(lecteur)
+        moteur = MoteurOuverture(log, porte)
+        moteur.interroger([lecteur])
+        moteur.interroger([lecteur])
         # ALORS cette porte ne s'ouvre pas
         self.assertEqual(1, porte.nb_appels_methode_ouvrir)
 
     def test_cas_plusieurs_portes(self):
         # ETANT DONNE un lecteur ayant detecté un badge
-        lecteur = LecteurFake()
-        lecteur.simuler_presentation_badge()
+        lecteur = LecteurFake(1)
+        lecteur.simuler_presentation_badge(1)
         # ET deux portes lui étant liées
         porte1 = PorteSpy()
         porte2 = PorteSpy()
+        log = LogSpy()
         # QUAND le moteur d'ouverture interroge deux fois ce lecteur
-        moteur = MoteurOuverture(porte1, porte2)
-        moteur.interroger(lecteur)
+        moteur = MoteurOuverture(log,porte1, porte2)
+        moteur.interroger([lecteur])
         # ALORS ces portes s'ouvrent
         self.assertEqual(1, porte1.nb_appels_methode_ouvrir)
         self.assertEqual(1, porte2.nb_appels_methode_ouvrir)
 
     def test_cas_plusieurs_lecteurs(self):
         # ETANT DONNE deux lecteurs ayant detecté un badge
-        lecteur1 = LecteurFake()
-        lecteur2 = LecteurFake()
-        lecteur2.simuler_presentation_badge()
+        lecteur1 = LecteurFake(1)
+        lecteur2 = LecteurFake(2)
+        lecteur2.simuler_presentation_badge(1)
         # ET deux portes lui étant liées
         porte = PorteSpy()
+        log = LogSpy()
         # QUAND le moteur d'ouverture interroge deux fois ce lecteur
-        moteur = MoteurOuverture(porte)
+        moteur = MoteurOuverture(log, porte)
         moteur.interroger([lecteur1, lecteur2])
         # ALORS ces portes s'ouvrent
         self.assertEqual(1, porte.nb_appels_methode_ouvrir)
 
     def test_detecter_badge_bloque(self):
         # ETANT DONNE un lecteur ayant détecté un badge bloqué
-        lecteur = LecteurFake()
+        lecteur = LecteurFake(1)
         lecteur.bloquer()
-        lecteur.simuler_presentation_badge()
+        lecteur.simuler_presentation_badge(1)
         # ET une porte lui étant liée
         porte = PorteSpy()
-        moteur = MoteurOuverture(porte)
+        log = LogSpy()
+        moteur = MoteurOuverture(log, porte)
         # QUAND le moteur d'ouverture interroge ce lecteur
         moteur.interroger([lecteur])
         # ALORS cette porte ne s'ouvre pas
@@ -98,13 +105,14 @@ class TestMain(unittest.TestCase):
 
     def test_detecter_badge_debloque(self):
         # ETANT DONNE un lecteur ayant détecté un badge bloqué
-        lecteur = LecteurFake()
+        lecteur = LecteurFake(1)
         lecteur.bloquer()
         lecteur.debloquer()
-        lecteur.simuler_presentation_badge()
+        lecteur.simuler_presentation_badge(1)
         # ET une porte lui étant liée
         porte = PorteSpy()
-        moteur = MoteurOuverture(porte)
+        log = LogSpy()
+        moteur = MoteurOuverture(log, porte)
         # QUAND le moteur d'ouverture interroge ce lecteur
         moteur.interroger([lecteur])
         # ALORS cette porte ne s'ouvre pas
@@ -112,8 +120,8 @@ class TestMain(unittest.TestCase):
 
     def test_detecter_badge_log_ok(self):
         # ETANT DONNE un lecteur ayant détecté un badge non bloqué
-        lecteur = LecteurFake()
-        lecteur.simuler_presentation_badge()
+        lecteur = LecteurFake(1)
+        lecteur.simuler_presentation_badge(1)
         log = LogSpy()
         # ET une porte lui étant liée
         porte = PorteSpy()
@@ -125,8 +133,8 @@ class TestMain(unittest.TestCase):
 
     def test_detecter_badge_log_ko(self):
         # ETANT DONNE un lecteur ayant détecté un badge bloqué
-        lecteur = LecteurFake()
-        lecteur.simuler_presentation_badge()
+        lecteur = LecteurFake(1)
+        lecteur.simuler_presentation_badge(1)
         lecteur.bloquer()
         log = LogSpy()
         # ET une porte lui étant liée
@@ -140,7 +148,7 @@ class TestMain(unittest.TestCase):
     def test_detecter_badge_log_id_lecteur(self):
         # ETANT DONNE un lecteur ayant détecté un badge non bloqué
         lecteur = LecteurFake(1)
-        lecteur.simuler_presentation_badge()
+        lecteur.simuler_presentation_badge(1)
         log = LogSpy()
         # ET une porte lui étant liée
         porte = PorteSpy()
